6fd9f310d866b3e56e2a256b88e8a2d46ae10e8c
Pankaj <pankajahir631@gmail.com>
2022-11-10 17:15:21 +0530

    working 10-11-2022 1

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ExperienceController;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
