adb81eb43e39680e8fb89dc2f9848b45f47366a0
Payal <payalpie0@gmail.com>
2022-12-31 13:53:02 +0530

    working chat module

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\ExperienceController;
-use App\Http\Controllers\API\UserController;
-use App\Http\Controllers\API\OrderController;
-use App\Http\Controllers\API\WishlistController;
+use App\Http\Controllers\API\{AuthController, ExperienceController, UserController, OrderController, WishlistController, ChatController};
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +19,7 @@ use App\Http\Controllers\API\WishlistController;
 //     return $request->user();
 // });
 
-Route::post('register_user',[UserController::class,'register_user']);
+Route::post('register_user', [UserController::class, 'register_user']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('verify_otp', [AuthController::class, 'verify_otp']);
 Route::post('forgetpassword', [AuthController::class, 'forgetpassword']);
@@ -31,59 +27,55 @@ Route::post('resetpassword', [AuthController::class, 'resetpassword']);
 
 
 
-Route::group(['middleware' => 'auth:api'], function(){
+Route::group(['middleware' => 'auth:api'], function () {
 
-    Route::post('addExperienceType', [ExperienceController::class,'addExperienceType']);
-    Route::post('addExperienceLocation', [ExperienceController::class,'addExperienceLocation']);
-    Route::post('addExperienceCategory', [ExperienceController::class,'addExperienceCategory']);
-    Route::post('addExperienceDetails', [ExperienceController::class,'addExperienceDetails']);
-    Route::post('addExperienceAgeGroup', [ExperienceController::class,'addExperienceAgeGroup']);
-    Route::post('addExperienceMedia', [ExperienceController::class,'addExperienceMedia']);
-    Route::post('addExperienceAgeGroup', [ExperienceController::class,'addExperienceAgeGroup']);
-    Route::post('addExperienceBrindItem', [ExperienceController::class,'addExperienceBrindItem']);
-    Route::post('addExperienceProvideItem', [ExperienceController::class,'addExperienceProvideItem']);
-    Route::post('addExperienceMeetLocation', [ExperienceController::class,'addExperienceMeetLocation']);
-    Route::post('addExperienceMaxGroupSize', [ExperienceController::class,'addExperienceMaxGroupSize']);
-    Route::post('addExperienceScheduleTime', [ExperienceController::class,'addExperienceScheduleTime']);
-    Route::post('addExperiencePrice', [ExperienceController::class,'addExperiencePrice']);
-    Route::post('addDiscountGroup', [ExperienceController::class,'addDiscountGroup']);
-    Route::post('addExperienceCancelletionPolicy', [ExperienceController::class,'addExperienceCancelletionPolicy']);
-    Route::post('addExperienceCategoryAttribute', [ExperienceController::class,'addExperienceCategoryAttribute']);
-    Route::get('removeExperience', [ExperienceController::class,'removeExperience']);
-    Route::get('getExperience', [ExperienceController::class,'getExperience']);
-    Route::get('getExperiences', [ExperienceController::class,'getExperiences']);
-    Route::get('otherlist',[ExperienceController::class,'otherlist']);
-    Route::get('city/{text}',[ExperienceController::class,'city']);
-    Route::get('removeMediaExperience', [ExperienceController::class,'removeMediaExperience']);
+    Route::post('addExperienceType', [ExperienceController::class, 'addExperienceType']);
+    Route::post('addExperienceLocation', [ExperienceController::class, 'addExperienceLocation']);
+    Route::post('addExperienceCategory', [ExperienceController::class, 'addExperienceCategory']);
+    Route::post('addExperienceDetails', [ExperienceController::class, 'addExperienceDetails']);
+    Route::post('addExperienceAgeGroup', [ExperienceController::class, 'addExperienceAgeGroup']);
+    Route::post('addExperienceMedia', [ExperienceController::class, 'addExperienceMedia']);
+    Route::post('addExperienceAgeGroup', [ExperienceController::class, 'addExperienceAgeGroup']);
+    Route::post('addExperienceBrindItem', [ExperienceController::class, 'addExperienceBrindItem']);
+    Route::post('addExperienceProvideItem', [ExperienceController::class, 'addExperienceProvideItem']);
+    Route::post('addExperienceMeetLocation', [ExperienceController::class, 'addExperienceMeetLocation']);
+    Route::post('addExperienceMaxGroupSize', [ExperienceController::class, 'addExperienceMaxGroupSize']);
+    Route::post('addExperienceScheduleTime', [ExperienceController::class, 'addExperienceScheduleTime']);
+    Route::post('addExperiencePrice', [ExperienceController::class, 'addExperiencePrice']);
+    Route::post('addDiscountGroup', [ExperienceController::class, 'addDiscountGroup']);
+    Route::post('addExperienceCancelletionPolicy', [ExperienceController::class, 'addExperienceCancelletionPolicy']);
+    Route::post('addExperienceCategoryAttribute', [ExperienceController::class, 'addExperienceCategoryAttribute']);
+    Route::get('removeExperience', [ExperienceController::class, 'removeExperience']);
+    Route::get('getExperience', [ExperienceController::class, 'getExperience']);
+    Route::get('getExperiences', [ExperienceController::class, 'getExperiences']);
+    Route::get('otherlist', [ExperienceController::class, 'otherlist']);
+    Route::get('city/{text}', [ExperienceController::class, 'city']);
+    Route::get('removeMediaExperience', [ExperienceController::class, 'removeMediaExperience']);
 
-    Route::post('checkorderslot', [OrderController::class,'checkorderslot']);
+    Route::post('checkorderslot', [OrderController::class, 'checkorderslot']);
 
-    Route::post('createorder', [OrderController::class,'createorder']);
-    Route::post('getHostOrders', [OrderController::class,'getHostOrders']);
-    Route::post('getMyOrders', [OrderController::class,'getMyOrders']);
+    Route::post('createorder', [OrderController::class, 'createorder']);
+    Route::post('getHostOrders', [OrderController::class, 'getHostOrders']);
+    Route::post('getMyOrders', [OrderController::class, 'getMyOrders']);
 
-    Route::get('getOrderCalender/{month}/{year}', [OrderController::class,'getOrderCalender']);
+    Route::get('getOrderCalender/{month}/{year}', [OrderController::class, 'getOrderCalender']);
 
-    Route::post('editProfile', [UserController::class,'editProfile']);
+    Route::post('editProfile', [UserController::class, 'editProfile']);
 
-    Route::post('update_wishlist',[WishlistController::class,'update_wishlist']);
-    Route::get('wishlistitem_list',[WishlistController::class,'wishlistitem_list']);
+    Route::post('update_wishlist', [WishlistController::class, 'update_wishlist']);
+    Route::get('wishlistitem_list', [WishlistController::class, 'wishlistitem_list']);
 
-
-    
-    
+    // pooja
+    Route::post('create-chat', [ChatController::class, 'CreateChat']);
+    Route::get('get-all-chat/{id}', [ChatController::class, 'GetAllChat']);
+    Route::get('personal-chat/{user_id}/{receiver_id}', [ChatController::class, 'PersonalChat']);
+    Route::get('unread-msg-count', [ChatController::class, 'UnreadMessageCount']);
+    Route::get('get_all_unread_msg_count', [ChatController::class, 'GetAllUnreadMessageCount']);
 });
 
-   Route::get('settings',[UserController::class,'settings']);
-Route::get('getHomeExperiences', [ExperienceController::class,'getHomeExperiences']);
-Route::get('experienceDetails/{id}', [ExperienceController::class,'experienceDetails']);
-Route::get('getRelatedExperiences/{id}', [ExperienceController::class,'getRelatedExperiences']);
-Route::get('getReviewExperiences/{id}', [ExperienceController::class,'getReviewExperiences']);
-Route::get('getAvailableTimeExperiences/{id}/{day}', [ExperienceController::class,'getAvailableTimeExperiences']);
-
-
-
-
-
-
-
+Route::get('settings', [UserController::class, 'settings']);
+Route::get('getHomeExperiences', [ExperienceController::class, 'getHomeExperiences']);
+Route::get('experienceDetails/{id}', [ExperienceController::class, 'experienceDetails']);
+Route::get('getRelatedExperiences/{id}', [ExperienceController::class, 'getRelatedExperiences']);
+Route::get('getReviewExperiences/{id}', [ExperienceController::class, 'getReviewExperiences']);
+Route::get('getAvailableTimeExperiences/{id}/{day}', [ExperienceController::class, 'getAvailableTimeExperiences']);
