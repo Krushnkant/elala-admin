a28b1cba8e57dcfb956e654a5614afa30aaa92a6
pankaj5604 <pankajahir631@gmail.com>
2023-02-08 13:00:58 +0530

    working 08-02-2023 8

--- app/Http/Controllers/API/ExperienceController.php
+++ app/Http/Controllers/API/ExperienceController.php
@@ -795,13 +795,14 @@ class ExperienceController extends BaseController
         $treding_experiences_arr = array();
         foreach ($treding_experiences as $experience){
 
-            
-            $maincategories = $this->getMainCategory($experience->category_id);
-            foreach($maincategories as $maincategory){
-                $ExperienceCategory = New ExperienceCategory();
-                $ExperienceCategory->experience_id = $experience->id;
-                $ExperienceCategory->category_id = $maincategory;
-                $ExperienceCategory->save();
+            if($experience->category_id > 0){
+                $maincategories = $this->getMainCategory($experience->category_id);
+                foreach($maincategories as $maincategory){
+                    $ExperienceCategory = New ExperienceCategory();
+                    $ExperienceCategory->experience_id = $experience->id;
+                    $ExperienceCategory->category_id = $maincategory;
+                    $ExperienceCategory->save();
+                }
             }
 
             $temp = array();
