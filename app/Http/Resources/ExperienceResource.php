9759be67abb6121c802287bd4160940329a6a39c
pankaj5604 <pankajahir631@gmail.com>
2023-02-17 14:22:51 +0530

    working 17-02-2023 5

--- app/Http/Resources/ExperienceResource.php
+++ app/Http/Resources/ExperienceResource.php
@@ -17,7 +17,7 @@ class ExperienceResource extends JsonResource
     public function toArray($request)
     {
        
-        return parent::toArray($request);
+        
         $ProvideItem = ExperienceProvideItem::where('experience_id',$this->id)->get(['id','title']);
         $BrindItem = ExperienceBrindItem::where('experience_id',$this->id)->get(['id','title']);
         $Images = ExperienceMedia::where('experience_id',$this->id)->where('type','img')->get(['id','thumb']);
@@ -27,7 +27,7 @@ class ExperienceResource extends JsonResource
         $city = City::where('id',$this->city)->first();
         $state = State::where('id',$this->state)->first();
         $country = Country::where('id',$this->country)->first();
-    
+        return parent::toArray($request);
         $attributes_arr = array();
         if($this->category_id != "" && $this->category_id != 0){
             $categoryAttribute= CategoryAttribute::with('attr_optioin')->where('category_id',$this->category_id)->get();
